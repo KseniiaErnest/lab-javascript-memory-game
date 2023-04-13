37a1404444d1e60b2ee60bc493fa224208125c4b
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-13 18:03:51 -0400

    Complete iterations #0 - 2.2

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,55 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
+    
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.cards.length; i++) {
+      let randomNum = Math.floor(Math.random() * (this.cards.length));
+
+      let tempHolder = this.cards[i];
+      this.cards[i] = this.cards[randomNum];
+      this.cards[randomNum] = tempHolder;
+    }
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1, card2) {
+      this.pairsClicked++;
+    } 
+    
+    if (card1 === card2) {
+      this.pairsGuessed++;
+    } 
+    
+    if (card1 === card2) {
+      return true;
+    } else if (card1 !== card2) {
+      return false
+    }
+    
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === 0) {
+      return false
+    } 
+    
+    
+    if (this.pairsGuessed < 24 / 2) {
+      return false
+    } else if (this.pairsGuessed === 24 / 2) {
+      return true
+    }   
   }
 }
